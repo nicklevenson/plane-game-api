b9d89404e3376dd03c18537aea4479d92621e3ff
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-10 17:45:59 -0800

    new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
+    user = User.find_or_create_by(user_params)
+ 
     render json: user
-    byebug
   end
 
   private
 
   def user_params
+
     params.require(:user).permit(:username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :scores[:create]
-  resources :users[:index, :create]
+  resources :scores, only: [:create]
+  resources :users, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
