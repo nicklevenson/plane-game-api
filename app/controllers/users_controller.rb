4becd818812c590b1bf4fc39cfea82252eb7130b
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-11 13:27:02 -0800

    user scores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
   def create
     user = User.find_or_create_by(user_params)
-    scores = user.scores.collect{|s|s.score}
+    scores = user.scores.collect{|s|s.score}.reverse
     if user.save
       render json: {username: user.username, id: user.id, scores: scores}
     else
       render json: {error: {message: "Username cannot be blank"}}, status: 400
     end
+  end
+
+  def show
+    user = User.find(params[:id])
+    scores = user.scores.collect{|s|s.score}.reverse
+    render json: {username: user.username, id: user.id, scores: scores}
   end
 
   private
