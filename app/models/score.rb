bc39b1942f724c47735ef12617327453d3493764
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-11 12:33:22 -0800

    score add

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,6 +2,6 @@ class Score < ApplicationRecord
   belongs_to :user
 
   def self.high_scores 
-    all.order(score: :asc).collect{|s|"#{s.user.username}: #{s.score}"}
+    all.order(score: :desc).collect{|s|"#{s.user.username}: #{s.score}"}
   end
 end
