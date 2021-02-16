2c137eee15a541b7830152991273a5b8ec77c6f3
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-16 10:30:17 -0800

    ordering

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,7 +1,9 @@
 class Score < ApplicationRecord
   belongs_to :user
 
+  default_scope {order(created_at: :asc).reorder(score: :desc)}
+
   def self.high_scores 
-    all.order(score: :desc).collect{|s|"#{s.user.username}: #{s.score}"}.take(25)
+    all.collect{|s|"#{s.user.username}: #{s.score}"}.take(25)
   end
 end
