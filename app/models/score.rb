0e0413eb16592db86cf547d637d2a7b77018582b
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-16 07:59:15 -0800

    top 25

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,6 +2,6 @@ class Score < ApplicationRecord
   belongs_to :user
 
   def self.high_scores 
-    all.order(score: :desc).collect{|s|"#{s.user.username}: #{s.score}"}.take(26)
+    all.order(score: :desc).collect{|s|"#{s.user.username}: #{s.score}"}.take(25)
   end
 end
